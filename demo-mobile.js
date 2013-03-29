abcdd73a443babbff80ec30716be4b73ac7d267c
Sébastien Drouyer <sdrdis@gmail.com>
2013-03-29 20:03:16 +0100

    added jquery mobile

--- demo-mobile.js
+++ demo-mobile.js
@@ -8,7 +8,6 @@ examples['simple'] = function() {
 
 function selectExample(example) {
   var $sphere = $('#sphere');
-  $sphere.earth3d('destroy');
   $sphere.replaceWith($('<canvas id="sphere"></canvas>').attr({
     width: $sphere.width(),
     height: $sphere.height()
@@ -16,20 +15,6 @@ function selectExample(example) {
   $('.location').remove();
   $('.flight').remove();
   $('#flights')[0].getContext('2d').clearRect(0, 0, 100, 100);
-  if (example == 'simple_mars') {
-    $('#glow-shadows').removeClass('earth').addClass('mars');
-  } else {
-    $('#glow-shadows').removeClass('mars').addClass('earth');
-  }
-  var code = examples[example].toString();
-  code = code.substring(14);
-  code = code.substring(0, code.length - 2);
-  var lines = code.split("\n");
-  for (var i = 0; i < lines.length; i++) {
-    lines[i] = lines[i].substring(2);
-  }
-  code = lines.join("\n");
-  $('#example_code').val(code);
 
   examples[example]();
 }
